310bc03540a71d224548ef5efa8a68dd6236a110
Angel Santos Ramirez <i72saraf@uco.es>
2019-04-11 10:47:58 +0200

    3

--- eje3.sh
+++ eje3.sh
@@ -1,10 +1,19 @@
-
 mkdir pequeños
 mkdir medianos
 mkdir grandes
 
-for x in $(find $1 -size "-$umbral1"c -type f) # peq
+for x in $(find $1 -size -$2c -type f) # peq
 do
 	cp	$x pequeños
 done
-;
+
+
+for x in $(find $1 -size +$2c -type f -o -size -$3c -type f) # peq
+do
+	cp	$x medianos
+done
+
+for x in $(find $1 -size +$3c -type f ) # peq
+do
+	cp	$x grandes
+done
--- eje5.sh
+++ eje5.sh
@@ -1,8 +1,29 @@
-date=$(date +%s)
-for x in $(find copia_seg -type f)
-do
-	creation=$(stat $x -c %x)
-	time=$date-$creation
-	if[] 
-done
-tar -czf copia_seg/copia-$(whoami) -$date.tar.gz $*
+
+if [ -d copia_seg ];
+then
+
+	date=$(date +%s)
+	for x in $(find  -type f -name $1)
+	do
+		creation=$(stat $x -c %x)
+		time=$date-$creation
+		if[] 
+	done
+	#tar -czf copia_seg/copia-$(whoami) -$date.tar.gz $*
+
+else
+	echo "No existe copia_seg , se creará"
+
+	mkdir copia_seg
+
+	date=$(date +%s)
+	for x in $(find copia_seg -type f)
+	do
+		creation=$(stat $x -c %x)
+		time=$date-$creation
+		if[ time > 200]; 
+	done
+	#tar -czf copia_seg/copia -$(whoami) -$date.tar.gz $*
+
+
+fi
